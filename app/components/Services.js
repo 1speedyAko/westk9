cf47c9f33d2e3e670d5de8f1e29554212a70b209
charlie <charloako1@gmail.com>
2025-03-19 07:21:54 +0300

    config services

--- app/components/Services.js
+++ app/components/Services.js
@@ -1,30 +1,35 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 function Services() {
   const services = [
     {
       title: "Dog Grooming",
       image: "/cougar.JPG",
-      details: "Nail clipping, Full body wash"
+      details: "Nail clipping, Full body wash",
+      route: "/dog-grooming",
     },
     {
       title: "Dog Training",
       image: "/blook.JPG",
-      details: "Basic to advanced training"
+      details: "Basic to advanced training",
+      route: "/dog-training",
     },
     {
       title: "Handler Training",
       image: "/taliban.JPG",
-      details: "Understanding behaviour and communication"
+      details: "Understanding behaviour and communication",
+      route: "/handler-training",
     },
     {
       title: "Dog Breeding",
       image: "/lookup.JPG",
-      details: "Stud services, Puppy rehoming"
-    }
+      details: "Stud services, Puppy rehoming",
+      route: "/dog-breeding",
+    },
   ];
 
   return (
@@ -32,24 +37,24 @@ function Services() {
       <h1 className="text-black text-3xl text-center pb-8 font-bold">Our Services</h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-7 px-4">
         {services.map((service, index) => (
-          <Card key={index}
-          className="shadow-lg overflow-hidden flex flex-col transform transition duration-300 hover:scale-105 hover:shadow-xl">
+          <Card key={index} className="shadow-lg overflow-hidden flex flex-col transform transition duration-300 hover:scale-105 hover:shadow-xl">
             <div className="relative w-full h-64">
               <Image src={service.image} alt={service.title} layout="fill" objectFit="cover" />
             </div>
             <CardContent className="p-4 text-center">
               <CardTitle className="text-xl font-bold">{service.title}</CardTitle>
               <p className="text-black text-sm mt-2">{service.details}</p>
-              <Button className="mt-4 transition duration-300 transform hover:scale-105 hover:bg-emerald-600 hover:text-white">Read More</Button>
-              </CardContent>
+              <Link href={service.route}>
+                <Button className="mt-4 transition duration-300 transform hover:scale-105 hover:bg-emerald-600 hover:text-white">
+                  Read More
+                </Button>
+              </Link>
+            </CardContent>
           </Card>
         ))}
       </div>
     </div>
   );
-    </div>
-  );
 }
 
-export default Services;
 export default Services;
